716e28ace00595179c2b3384aa997c7f3ecaae29
aylin-tech <114916346+aylin-tech@users.noreply.github.com>
2024-11-17 13:44:33 +0900

    Add files via upload

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -10,176 +10,175 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(0.0),
-          child: Row(
-            children: [
-              // Left side with the image
-              Expanded(
-                flex: 1,
-                child: Center(
-                  child: Image.asset('assets/images/travel.png', height: 1080),
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Padding(
-                  padding: EdgeInsets.zero,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Travel Mate',
-                        style: TextStyle(
-                          fontSize: 55,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'MuseoModerno',
-                          color: Colors.black,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 32,
-                        width: 341,
-                        child: Text(
-                          '나를 위한 여행 플래너, 트래블메이트',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.black38,
-                            fontFamily: 'Montserrat',
-                          ),
-                        ),
-                      ),
-
-                      SizedBox(height: 10),
-
-                      SizedBox(
-                        height: 110,
-                        width: 426,
-                        child: Text(
-                          '단 하나 뿐인 당신의 여행을 만들어드립니다.\n당신의 모든 요구를 만족시키는 맞춤 설계 여행,\n이제 간편하게 시작하세요.',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.black54,
-                            fontFamily: 'Montserrat',
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: (){
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => HomePage()),
-                          );
-                        },
-                        child: Text(
-                          '바로 시작하기',
-                          style: TextStyle(
-                            fontSize: 24,
-                            color: Colors.white,
-                          ),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          padding: EdgeInsets.zero,
-                          minimumSize: Size(440, 75),
-                          backgroundColor: Color(0xFF689ADB),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              width: 180,
-                              child: Divider(
-                                thickness: 1,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 10),
-                              child: Text(
-                                'or',
-                                style: TextStyle(fontSize: 16, color: Colors.black),
-                              ),
-                            ),
-                            Container(
-                              width: 180,
-                              child: Divider(
-                                thickness: 1,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      // Login Button
-                      OutlinedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => LoginPage()),
-                          );
-                        },
-                        child: Text(
-                          '로그인',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Color(0xFF1E4577),
-                          ),
-                        ),
-                        style: OutlinedButton.styleFrom(
-                          padding: EdgeInsets.zero,
-                          minimumSize: Size(440, 48),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      OutlinedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => SignupPage()),
-                          );
-                        },
-                        child: Text(
-                          '회원가입',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Color(0xFF1E4577),
-                          ),
-                        ),
-                        style: OutlinedButton.styleFrom(
-                          padding: EdgeInsets.zero,
-                          minimumSize: Size(440, 48),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10),
-                        child: Text(
-                          '계정을 만들면서 로그인해주세요!',
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey,
-                            fontFamily: 'Montserrat',
-                          ),
-                        ),
-                      ),
-                    ],
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/travel.png'),
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.white,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Travel Mate',
+                      style: TextStyle(
+                        fontSize: 55,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'MuseoModerno',
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 32,
+                      width: 341,
+                      child: Text(
+                        '나를 위한 여행 플래너, 트래블메이트',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black38,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
+                    ),
+                    SizedBox(height:3),
+                    SizedBox(
+                      height: 120,
+                      width: 426,
+                      child: Text(
+                        '단 하나 뿐인 당신의 여행을 만들어드립니다.\n당신의 모든 요구를 만족시키는 맞춤 설계 여행,\n이제 간편하게 시작하세요.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize:16 ,
+                          color: Colors.black,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomePage()),
+                        );
+                      },
+                      child: Text(
+                        '바로 시작하기',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.zero,
+                        minimumSize: Size(440, 75),
+                        backgroundColor: Color(0xFF689ADB),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 180,
+                            child: Divider(
+                              thickness: 1,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Text(
+                              'or',
+                              style: TextStyle(fontSize: 16, color: Colors.black),
+                            ),
+                          ),
+                          Container(
+                            width: 180,
+                            child: Divider(
+                              thickness: 1,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    OutlinedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginPage()),
+                        );
+                      },
+                      child: Text(
+                        '로그인',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Color(0xFF1E4577),
+                        ),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                        padding: EdgeInsets.zero,
+                        minimumSize: Size(440, 48),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    OutlinedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SignupPage()),
+                        );
+                      },
+                      child: Text(
+                        '회원가입',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Color(0xFF1E4577),
+                        ),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                        padding: EdgeInsets.zero,
+                        minimumSize: Size(440, 48),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Text(
+                        '계정을 만들면서 로그인해주세요!',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
